524c5d0acad6acb3cbc5b1222ab6c179e8fb5260
BiscuiTech <jhuet@biscui.tech>
2021-12-02 21:11:43 -0500

    Solved day 1

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,42 @@ fn main() -> std::io::Result<()> {
 }
 
 fn day_1() {
+    // PART 1
     // read file from disk
     let contents = fs::read_to_string("src/day1/readings.txt").unwrap();
-    println!("{}", contents);
+    let split_string: Vec<&str> = contents.split("\n").collect();
+    // convert string to int
+    let readings: Vec<i32> = split_string
+        .iter()
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect();
+    // iterate over readings
+    let mut total_increases = 0;
+    for i in 0..readings.len() {
+        if i > 0 && readings[i] > readings[i - 1] {
+            total_increases += 1;
+        }
+    }
+    println!("part 1: {:?}", total_increases);
+
+    // PART 2
+    // iterate over readings
+    let mut total_combined_increases = 0;
+
+    for i in 0..readings.len() {
+        // check if there are enough items to compare (4 items)
+
+        if readings.len() - i >= 4 {
+            // combine the first 3 items
+            let mut low_combined_readings = readings[i] + readings[i + 1] + readings[i + 2];
+            let mut high_combined_readings = readings[i + 1] + readings[i + 2] + readings[i + 3];
+            // check if the high readings are higher than the low readings
+            if high_combined_readings > low_combined_readings {
+                total_combined_increases += 1;
+            }
+        } else {
+            break;
+        }
+    }
+    println!("part 2: {:?}", total_combined_increases);
 }
